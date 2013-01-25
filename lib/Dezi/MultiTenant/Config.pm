516e91cff52f0c97cad69051b87b6eeaa5d46b5e
Peter Karman <karpet@peknet.com>
2013-01-24 21:56:41 -0600

    add config docs

--- lib/Dezi/MultiTenant/Config.pm
+++ lib/Dezi/MultiTenant/Config.pm
@@ -9,6 +9,49 @@ use Plack::Util::Accessor qw(
 
 our $VERSION = '0.001';
 
+=head1 NAME
+
+Dezi::MultiTenant::Config - multi-tenant Dezi configuration
+
+=head1 SYNOPSIS
+
+ use Dezi::MultiTenant::Config;
+
+ my $dmc = Dezi::MultiTenant::Config->new(
+     '/foo' => {
+         engine_config => {
+             type => 'Lucy'
+         },
+     },
+     '/bar' => {
+         engine_config => {
+             type => 'Xapian',
+         },
+     },
+ });
+
+ for my $path ($dmc->paths()) {
+    printf("%s mounted with config: %s\n",
+        $path, Data::Dump::dump( $dmc->config_for( $path )
+    );
+ }
+
+=head1 DESCRIPTION
+
+Dezi::MultiTenant::Config is a hash-based configuration
+with keys representing Plack mount points and values of
+Dezi::Config objects.
+
+=head1 METHODS
+
+=head2 new( I<config> )
+
+I<config> should be a hashref with keys representing
+Plack mount points. Values are passed directly
+to Dezi::Config->new().
+
+=cut
+
 sub new {
     my $class = shift;
     my $config = shift or croak "config hashref required";
@@ -24,10 +67,22 @@ sub new {
     return bless { configs => \%configs }, $class;
 }
 
+=head2 paths
+
+Returns array of keys set in new().
+
+=cut
+
 sub paths {
     my $self = shift;
     return keys %{ $self->configs };
 }
+
+=head2 config_for( I<path> )
+
+Returns Dezi::Config object for I<path>.
+
+=cut
 
 sub config_for {
     my $self = shift;
